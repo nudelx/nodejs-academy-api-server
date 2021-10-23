3b4a9251feedf20e4a40db5db9aa4c18c8fc5930
nudelx <alex@equifina.com>
2021-10-23 10:52:23 +0300

    async example

--- async.js
+++ async.js
@@ -50,17 +50,56 @@ const asyncMakerPromise = function (fn) {
 // .map((f) => asyncMaker(f))
 // .map((f) => f())
 
+
+
+const getMovie = () => new Promise((yes, no) => {
+  setTimeout(() => yes('movies {...}'), 2000)
+})
+const getActor = () => new Promise((yes, no) => {
+  setTimeout(() => yes('actors {...}'), 3000)
+})
+const getRating = () => new Promise((yes, no) => {
+  setTimeout(() => yes('rating {...}'), 4000)
+})
+
 const testCb = function () {
   return 1344 /32
 }
 
 
-const asyncTestWithCb = asyncMaker(testCb)
+// const asyncTestWithCb = asyncMaker(testCb)
 
-asyncTestWithCb(function (data) {
-  console.log( `this is the answer ${data}`)
+// asyncTestWithCb(function (data) {
+//   console.log( `this is the answer ${data}`)
+// })
+
+// const fetchData(url)
+// .then(data => setData(data))  //  or .then(setData)
+// .catch(console.error)    
+
+Promise.allSettled([getMovie(), getActor(), getRating()]).then((values) => {
+  console.log(values);
+}).catch(e => {
+  console.log(e)
 })
+  const getData = async function () {
+    const movieId = await getMovie(42)
+    const actorId = await getActor(movieId)
+    const rating  = await getRating(actorId)
+    return [movieId, actorId, rating]
+  }
+getMovie(42)
+.then(id => getActor(id)) // .then(getActor)
+.then(id => getRating(id)) // .then(getRating)
+.catch(console.error)
 
-const fetchData(url)
-.then(data => setData(data))  //  or .then(setData)
-.catch(console.error)    
+
+const test =  async function () { 
+  console.log('this is a test') 
+  return 'this is the returned value 42'
+}
+getData()
+.then(data => console.log(data))
+.then(()=> console.log('after'))
+.then(()=> console.log('after after'))
+.then(()=> console.log('after after after'))
