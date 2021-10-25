5986505708fa39b6f0e4f68223cfef7baeae4d0f
Erez Hod <erez@erezhod.com>
2021-10-25 16:11:50 +0300

    - Added .eslintrc.js to .prettierignore - Added rules to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
   },
-  'extends': [
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaVersion': 13,
+  extends: ['google'],
+  parserOptions: {
+    ecmaVersion: 13,
   },
-  'rules': {
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
   },
 };
