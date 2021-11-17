29857407264a19e2609d425aa930208d76dc555e
nudelx <alex@equifina.com>
2021-11-17 15:10:26 +0200

    mongoDriver

--- db/mongoDriver.js
+++ db/mongoDriver.js
@@ -5,7 +5,6 @@ const client = new MongoClient(uri)
 
 const listDatabases = async (client) => {
   databasesList = await client.db().admin().listDatabases()
-
   console.log('Databases:')
   databasesList.databases.forEach((db) => console.log(` - ${db.name}`))
 }
