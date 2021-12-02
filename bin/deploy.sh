e69885701ebd053be1a3d4d68b019f6b9fcf577a
nudelx <alex@equifina.com>
2021-12-02 16:56:45 +0200

    clean

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-clear
-echo " 👷‍♂️  Staring Movie API Deploy "
-git checkout main
-git pull
-git checkout deploy
-git pull
-git merge -X theirs main
-git commit -am "version update $(date)"
-npm run test
-echo " bla bla $? " 
+# clear
+# echo " 👷‍♂️  Staring Movie API Deploy "
+# git checkout main
+# git pull
+# git checkout deploy
+# git pull
+# git merge -X theirs main
+# git commit -am "version update $(date)"
+# npm run test
+# echo " bla bla $? " 
 
-# git add .
-# git commit -am "new deploy $(date)"
-# git push heroku deploy
+# # git add .
+# # git commit -am "new deploy $(date)"
+# # git push heroku deploy
 
-git checkout main
+# git checkout main
