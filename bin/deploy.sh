8dae92fb3605e6408d1892ae84d4099913c074c7
nudelx <alex@equifina.com>
2021-12-02 16:08:45 +0200

    test

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
-clear
-echo " 👷‍♂️  Staring Movie API Deploy "
-git checkout main
-git pull
-git checkout deploy
-git pull
-git merge -X theirs main
-git commit -am "version update $(date)"
-npm run test
+# clear
+# echo " 👷‍♂️  Staring Movie API Deploy "
+# git checkout main
+# git pull
+# git checkout deploy
+# git pull
+# git merge -X theirs main
+# git commit -am "version update $(date)"
+
+
+if npm run test
+then
+  STATEMENTS
+fi
 echo " bla bla $? " 
 
 # git add .
