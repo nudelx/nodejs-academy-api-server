028e616c7903101c03984611ba2a1ed0bd1af1ec
nudelx <alex@equifina.com>
2021-12-02 11:16:46 +0200

    my first deploy

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,8 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
+        const { _id } = decoded
+        const user = await User.findOne({ _id, 'tokens.token': token })
 
         if (!user) {
             throw new Error()
