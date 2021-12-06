d1ff05ac44de4e3da74a8e893a03321f028fd69e
nudelx <alex@equifina.com>
2021-12-06 11:10:12 +0200

    check jwt secret

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,18 @@
 const jwt = require('jsonwebtoken')
 const { User } = require('../db')
 const log = require('../utils/logger')
+const { JWT_SECRET } = process.env
 const auth = async (req, res, next) => {
   try {
     log('Auth MDW', 'auth')
     const token = req.header('Authorization').replace('Bearer ', '')
-    log(`Auth MDW - token: ${token}`, 'auth')
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    log(`Auth MDW - token: ${token}`, 'bad')
+    !JWT_SECRET && log(`Auth MDW - JWT_SECRET is not defined `, 'auth')
+    const decoded = jwt.verify(token)
     const { _id } = decoded
     const user = await User.findOne({ _id, token })
     if (!user) {
-        log(`Auth MDW - no user`, 'auth')
+      log(`Auth MDW - no user`, 'auth')
       throw new Error('Authorization user not found')
     }
     req.user = user
